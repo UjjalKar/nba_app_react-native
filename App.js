b27f3223c7e14a9655401cdb9a535691155de9a4
Ujjal Kar <ujjalkar17@gmail.com>
2019-10-11 20:46:05 +0530

    'init

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <Text>Hello React</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
